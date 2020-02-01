31021f3835e1581c30ca7a9dccedc6ece4c87fd0
Seth Lachman <lachmanseth17@gmail.com>
2020-02-01 09:48:23 -0500

    Added timer to recipe page.

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -9,24 +9,67 @@ const Recipe = () => {
       .split("-")
       .map(word => word[0].toUpperCase() + word.slice(1))
       .join(" ");
+  const convertSeconds = time => {
+    let minutes = Math.floor(time / 60);
+    let seconds = time % 60;
+    if (minutes < 1) {
+      minutes = "00";
+    }
+    if (seconds < 10) {
+      seconds = "0" + seconds;
+    }
+    if (seconds === 0) {
+      seconds = "00";
+    }
+
+    return `${minutes}:${seconds}`;
+  };
+
   let { recipeId } = useParams();
   const [recipe, setRecipe] = useState("");
   const [grams, setGrams] = useState(20);
   const [ratio, setRatio] = useState(15);
   const [time, setTime] = useState(2);
+  const [currentTime, setCurrentTime] = useState(null);
+  const [toggleTimer, setToggleTimer] = useState(false);
 
   useEffect(() => {
     setRecipe(convertToName(recipeId));
   }, [recipeId]);
 
+  useEffect(() => {
+    let id;
+    if (toggleTimer) {
+      if (currentTime === null) {
+        setCurrentTime(time * 60);
+      } else if (currentTime > 0) {
+        id = setInterval(() => {
+          setCurrentTime(currentTime - 1);
+        }, 1000);
+      } else {
+        setTime(time);
+        setToggleTimer(!toggleTimer);
+        setCurrentTime(null);
+        clearInterval(id);
+      }
+    }
+    return () => clearInterval(id);
+  }, [toggleTimer, currentTime, time]);
+
+  const startTimer = e => {
+    e.preventDefault();
+    setToggleTimer(!toggleTimer);
+  };
+
   return (
     <>
       <Flex flexDirection="column" my={3} alignItems="center">
         <Text as="h2">{recipe}</Text>
+        {currentTime && <Text my={2}>Time: {convertSeconds(currentTime)}</Text>}
         <Text my={2}>Water Needed: {grams * ratio}ml</Text>
       </Flex>
       <Flex justifyContent="center">
-        <Box as="form" onSubmit={e => e.preventDefault()} py={3}>
+        <Box as="form" onSubmit={startTimer} py={3}>
           <Flex mx={-2} mb={3}>
             <Box width={1 / 2} px={2}>
               <Label htmlFor="ratio">Grams</Label>
@@ -65,7 +108,7 @@ const Recipe = () => {
           </Flex>
           <Flex mx={-2} flexWrap="wrap">
             <Box px={2} ml="auto">
-              <Button bg="black">Start Brew</Button>
+              <Button bg="black">{toggleTimer ? "Stop" : "Start"} Brew</Button>
             </Box>
           </Flex>
         </Box>
